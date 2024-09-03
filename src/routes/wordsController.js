159ffb37fe11a7c6dc9d70cee7d6a20c7ff67d4b
Jakob <67110757+spj2401Dev@users.noreply.github.com>
2024-09-03 11:46:18 +0200

    ~ /words route can now handle more than 25 words in the word list

--- src/routes/wordsController.js
+++ src/routes/wordsController.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const words = require('../utilities/wordsUtility');
 
 router.get('/words', (req, res) => {
-    res.json(words.sort(() => Math.random() - 0.5));
+    res.json(words.sort(() => Math.random() - 0.5).slice(0, 25));
 });
 
 module.exports = router;
