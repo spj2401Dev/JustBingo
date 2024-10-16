229b57490f6134d7bb74c113a9e4ca866ccf4a55
spj2401Dev <jakob.joas@fideliushaus.com>
2024-10-16 20:56:19 +0200

    Fields Update

--- src/routes/wordsController.js
+++ src/routes/wordsController.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const editJsonFile = require("edit-json-file");
 
-let file = editJsonFile(`${__dirname}/../../words.json`);
+const { getWords } = require('../services/apiDataService');
 
 router.get('/words', (req, res) => {
-    var words = file.get('words') || [];
-    res.json(words.sort(() => Math.random() - 0.5).slice(0, 25));
+    res.json(getWords());
 });
+
 
 module.exports = router;
